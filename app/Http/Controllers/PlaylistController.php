1d435b291270d2a0e116efd8b45f21547cb69437
wang.yuan <wang.yuan@joydata.com>
2020-09-21 13:01:20 +0800

    update

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -184,7 +184,7 @@ class PlaylistController extends AppBaseController
 
         Flash::success('Channel saved successfully.');
 
-        return redirect(route('channels.index'));
+        return redirect(route('playlist.index'));
     }
 
 
