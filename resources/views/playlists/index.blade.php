7e1ec624d47fe42c3b3ae27271a47d0c8ab9195c
wang.yuan <wang.yuan@joydata.com>
2020-09-21 13:36:22 +0800

    update

--- resources/views/playlists/index.blade.php
+++ resources/views/playlists/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="float-left">Playlists</h1>
         <h1 class="float-right">
-            <a class="btn btn-primary" href="{!! route('fxz.index') !!}"><i class="fas fa-list"></i> 分享者 /av /test</a>
+            <a class="btn btn-secondary" href="{!! route('fxz.index') !!}"><i class="fas fa-list"></i> 分享者 /av /test</a>
 
             <a class="btn btn-primary" href="{!! route('playlists.create') !!}"><i class="fas fa-plus-circle"></i> Add New</a>
         </h1>
