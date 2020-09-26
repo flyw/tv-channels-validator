474cf670d289c48d822c4b7440daa706c229ec08
wang.yuan <wang.yuan@joydata.com>
2020-09-26 14:12:10 +0800

    update

--- app/Http/Controllers/FxzController.php
+++ app/Http/Controllers/FxzController.php
@@ -22,7 +22,7 @@ class FxzController extends Controller
 
     public function indexTest() {
         $playlist = Playlist::orderBy("priority", "ASC")->get();
-        $this->showList($playlist, true);
+        $this->showListDebug($playlist);
     }
 
 
@@ -30,6 +30,42 @@ class FxzController extends Controller
 
 
     private function showList($playlist, $test = false) {
+
+        $nullItem = [];
+        $nullItem['id'] = null;
+        $nullItem['name'] = "未分类";
+
+        $playlist->push($nullItem);
+        $playlistArray = $playlist->mapWithKeys(function ($item) {
+            return [$item['id'] => $item['name']];
+        })->toArray();
+        foreach ($playlistArray as $playlistId => $playlistName) {
+            echo "\n".'$c_start'.$playlistName.'$c_end'."\n";
+            if ($playlistId != null) {
+                $builder = Channel::where("playlist_id", $playlistId);
+            }
+            else {
+                $builder = Channel::whereNull("playlist_id");
+            }
+
+            $builder = $builder ->orderBy("valid", "DESC")
+                ->orderBy("name", "ASC");
+            $builder = $builder->where("valid", 1);
+
+            $prevName = "";
+            foreach ($builder->cursor() as $item) {
+                if ($prevName == $item->name) {
+                    echo "#".$item->url;
+                } else {
+                    echo "\n".$item->name.",".$item->url;
+                }
+                $prevName = $item->name;
+            }
+        }
+    }
+
+
+    private function showListDebug($playlist) {
 
         $nullItem = [];
         $nullItem['id'] = null;
@@ -51,12 +87,9 @@ class FxzController extends Controller
             $builder = $builder ->orderBy("valid", "DESC")
                 ->orderBy("name", "ASC");
 
-            if ($test == false) {
-                $builder = $builder->where("valid", 1);
-            }
             foreach ($builder->cursor() as $item) {
                 $valid = ($item->valid == 0)?"(失效)":"";
-                echo $item->name.$valid.",".$item->url."\n";
+                echo $item->name.$valid."($item->scheme $item->domain),".$item->url."\n";
             }
         }
     }
